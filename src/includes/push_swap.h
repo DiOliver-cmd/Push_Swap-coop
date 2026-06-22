fbbd2e72c4e226c83431d06ea606db36636787ab
Dilaine Ferreira De Oliveira <dilferre@c1r10p2.42sp.org.br>
2026-06-22 11:16:54 -0300

    inclusão de struct e ajuste de norma do header

--- src/includes/push_swap.h
+++ src/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: masalaib <masalaib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 19:18:19 by masalaib          #+#    #+#             */
-/*   Updated: 2026/06/18 20:09:54 by masalaib         ###   ########.fr       */
+/*   Updated: 2026/06/22 11:16:28 by dilferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 # define SUC 1
 # define MAX_LEN_WORD 11
 
-#include "../../libft/libft.h"
-#include "stdio.h"
+# include "../../libft/libft.h"
+# include "stdio.h"
 
-typedef struct s_node 
+typedef struct s_node
 {
-  int             value;
-  struct s_node   *next;
-}  t_node;
+	int				value;
+	struct s_node	*next;
+}	t_node;
 
 #endif
