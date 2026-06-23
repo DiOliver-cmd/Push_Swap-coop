3293eaaf406b09f9aa9e25139e8af07b44c4f2fe
Dilaine Ferreira De Oliveira <dilferre@c2r6p4.42sp.org.br>
2026-06-23 13:21:13 -0300

    norma run_swap

--- src/Run/run_swap.c
+++ src/Run/run_swap.c
@@ -6,8 +6,27 @@
 /*   By: dilferre <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 11:34:37 by dilferre          #+#    #+#             */
-/*   Updated: 2026/06/23 11:34:39 by dilferre         ###   ########.fr       */
+/*   Updated: 2026/06/23 13:20:30 by dilferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+void	sa(t_node **stack_a)
+{
+	t_node	*first;
+	t_node	*second;
+
+	if (!stack_a || *stack_a || !(*stack_a)->next)
+		return ;
+	first = *stack_a;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	second->prev = first->prev;
+	first->prev = second;
+	if (first->next)
+	first->next->prev = first;
+	*stack_a = second;
+	write(1, "sa\n", 3);
+}
